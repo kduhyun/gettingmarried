e79e6dfb57a8be51e16aa5b13178fb2fd82da80a
donghwan <donghwan_seo@tmax.co.kr>
2015-04-16 17:22:54 +0900

    fix typo

--- js/kakaolink.js
+++ js/kakaolink.js
@@ -4,7 +4,7 @@ Kakao.init('48d26996831aea00b4cc73e7b14a7873');
 // 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
 Kakao.Link.createTalkLinkButton({
 container: '#kakao-link-btn',
-label: '서동환♡손유진\n저희 결혼합니다\n5월17일 오후1시30분\n더 컨벤션 교통회관\n3층 아무르홀',
+label: '서동환♡손유진\n저희 결혼합니다\n5월17일 오후1시30분\n더 컨벤션 교통회관\n3층 아모르홀',
 image: {
 src: 'http://donghwanyujin.github.io/gettingmarried/img/katalk_icon.png',
 width: '300',
